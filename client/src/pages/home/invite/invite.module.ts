132cade1a1c6d5416a5a7842dfe3cc5d14ceb49b
Cain <cainwatson@users.noreply.github.com>
2018-07-19 22:34:22 -0500

    (update) invite friends on invite page

--- client/src/pages/home/invite/invite.module.ts
+++ client/src/pages/home/invite/invite.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { InvitePage } from './invite';
+import { ComponentsModule } from '../../../components/components.module';
 
 @NgModule({
   declarations: [
     InvitePage,
   ],
   imports: [
+    ComponentsModule,
     IonicPageModule.forChild(InvitePage),
   ],
 })
--- client/src/pages/home/invite/invite.ts
+++ client/src/pages/home/invite/invite.ts
@@ -1,41 +1,84 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IonicPage, NavParams, ViewController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../../../interfaces/User';
+import { AppState } from '../../../store/reducers';
 import { PartyProvider } from '../../../providers/party/party';
-import { Contacts } from '@ionic-native/contacts';
+import { LoadingUiProvider } from '../../../providers/loading-ui/loading-ui';
 
 @IonicPage()
 @Component({
   selector: 'page-invite',
   templateUrl: 'invite.html',
 })
-export class InvitePage {
+export class InvitePage implements OnInit {
 
-  phoneNumber: string
   partyId: string
+  partyGuests: User[]
+  friends$: Observable<User[]>
+  friends: User[]
+  searchQuery: string = ''
 
   constructor(
     public navParams: NavParams,
-    private contacts: Contacts,
+    public viewCtrl: ViewController,
     public partyProvider: PartyProvider,
+    public loadingUIProvider: LoadingUiProvider,
+    private store: Store<AppState>,
   ) {
     const party = this.navParams.get('party');
     this.partyId = party.id;
   }
 
-  getContact() {
-    this.contacts.pickContact()
-      .then(data => data.phoneNumbers[0].value)
-      .then(phoneNumber => this.phoneNumber = phoneNumber);
+  ngOnInit() {
+    this.friends$ = this.store.select('friends');
+    this.searchReset();
   }
 
-  addNumber() {
-    this.partyProvider.inviteUser(this.partyId, this.phoneNumber)
-      .then(() => this.phoneNumber = '')
-      .catch(e => console.log('Oops', e.message))
+  inviteFriend(friend) {
+    this.loadingUIProvider.load(
+      async () => {
+        await this.partyProvider.joinParty(friend.id, this.partyId);
+        await this.partyProvider.inviteUser(this.partyId, friend.phone);
+        this.friends = await this.onlyUninvitedUsers(this.friends);
+      },
+      `Something went wrong when inviting @${friend.username}`,
+      {
+        loadingOptions: { content: `Inviting @${friend.username}` },
+        delay: 500,
+      },
+    );
+  }
+
+  searchFriends() {
+    this.friends$.subscribe(friends => this.onlyUninvitedUsers(friends).then((uninvitedFriends) => {
+      this.friends = uninvitedFriends.filter(({ username, nickname }) => {
+        const query = this.searchQuery.toLowerCase();
+
+        username = username.toLowerCase();
+        nickname = nickname.toLowerCase();
+
+        return username.includes(query) || nickname.includes(query);
+      });
+    }));
+  }
+
+  searchReset() {
+    this.friends$.take(1).subscribe(friends => this.onlyUninvitedUsers(friends)
+      .then(uninvitedFriends => this.friends = uninvitedFriends));
+  }
+
+  onlyUninvitedUsers(friends): Promise<User[]> {
+    return this.partyProvider.getPartyUsers(this.partyId).then((partyGuestsResponse) => {
+      const partyGuestsIds = partyGuestsResponse.data.map(groupUser => groupUser.user.id);
+      return friends.filter(friend => !partyGuestsIds.includes(friend.id));
+    });
   }
 
   onDone() {
-    this.navParams.get('onDone')();
+    const onDone = this.navParams.get('onDone');
+    onDone ? onDone() : this.viewCtrl.dismiss();
   }
 
 }
