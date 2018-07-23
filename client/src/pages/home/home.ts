921b92473261684ea30c2798661c4706969234fb
Cain <cainwatson@users.noreply.github.com>
2018-07-22 20:55:23 -0500

    (add) update sidebar parties on invited

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -7,7 +7,9 @@ import { AppState } from '../../store/reducers';
 import { AddFriends } from '../../store/friends/friends.actions';
 import { FriendRequestsState } from '../../store/friend-requests/friend-requests';
 import { AddFromFriendRequests, AddToFriendRequests } from '../../store/friend-requests/friend-requests.actions';
+import { AddUserParties } from '../../store/parties/parties.actions';
 import { UserProvider } from '../../providers/user/user';
+import { PartyProvider } from '../../providers/party/party';
 import { app } from '../../feathers';
 
 @IonicPage()
@@ -26,13 +28,18 @@ export class HomePage implements OnInit, OnDestroy {
   user$: Observable<User>
   friendRequests$: Observable<FriendRequestsState>
 
-  constructor(public userProvider: UserProvider, private store: Store<AppState>) { }
+  constructor(
+    public userProvider: UserProvider,
+    public partyProvider: PartyProvider,
+    private store: Store<AppState>,
+  ) { }
 
   ngOnInit() {
     app.service('friends').on('created', this.updateFriendships.bind(this));
     app.service('friends').on('removed', this.updateFriendships.bind(this));
     app.service('friend-requests').on('created', this.updateFriendRequests.bind(this));
     app.service('friend-requests').on('removed', this.updateFriendRequests.bind(this));
+    app.service('group-users').on('created', this.updateParties.bind(this));
 
     this.user$ = this.store.select('user');
     this.friendRequests$ = this.store.select('friend-requests');
@@ -43,6 +50,7 @@ export class HomePage implements OnInit, OnDestroy {
     app.service('friends').off('removed', this.updateFriendships.bind(this));
     app.service('friend-requests').off('created', this.updateFriendRequests.bind(this));
     app.service('friend-requests').off('removed', this.updateFriendRequests.bind(this));
+    app.service('group-users').off('created', this.updateParties.bind(this));
   }
 
   updateFriendships(data) {
@@ -69,6 +77,18 @@ export class HomePage implements OnInit, OnDestroy {
           .then(friendRequestsResponse => this.store.dispatch(new AddFromFriendRequests(friendRequestsResponse.data)));
         this.userProvider.getToFriendRequests(userId)
           .then(friendRequestsResponse => this.store.dispatch(new AddToFriendRequests(friendRequestsResponse.data)));
+      }
+    });
+  }
+
+  updateParties(data) {
+    this.user$.take(1).subscribe((user) => {
+      const groupUserId = data.user_id;
+      const userId = user.id;
+
+      if (groupUserId === userId) {
+        this.partyProvider.getUserParties(userId)
+          .then(parties => this.store.dispatch(new AddUserParties(parties)));
       }
     });
   }
--- client/src/pages/home/sidebar/sidebar.ts
+++ client/src/pages/home/sidebar/sidebar.ts
@@ -1,41 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { Store } from '@ngrx/store';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/switchMap';
 
-import { User } from '../../../interfaces/User';
 import { Party } from '../../../interfaces/Party';
 import { AppState } from '../../../store/reducers';
 import { PartyProvider } from '../../../providers/party/party';
+import { Subscription } from '../../../../node_modules/rxjs/Subscription';
 
 @IonicPage()
 @Component({
   selector: 'page-sidebar',
   templateUrl: 'sidebar.html',
 })
-export class SidebarPage {
+export class SidebarPage implements OnInit, OnDestroy {
 
   query: string = ''
   parties: Array<Party> = []
 
-  user$: Observable<User>
   parties$: Observable<Party[]>
+  partiesSub: Subscription
 
   constructor(
     public navCtrl: NavController,
     public partyProvider: PartyProvider,
-    store: Store<AppState>,
-  ) {
-    this.user$ = store.select('user');
-    this.parties$ = store.select('parties');
-    this.parties$.subscribe((parties) => {
+    public changeDetectorRef: ChangeDetectorRef,
+    private store: Store<AppState>,
+  ) { }
+
+  ngOnInit() {
+    this.parties$ = this.store.select('parties');
+    this.partiesSub = this.parties$.subscribe((parties) => {
       this.parties = parties.sort((a, b) => {
         if (a.name === b.name) return 0;
         return a.name > b.name ? 1 : -1;
       });
+      this.changeDetectorRef.detectChanges();
     });
+  }
+
+  ngOnDestroy() {
+    this.partiesSub.unsubscribe();
+    this.changeDetectorRef.detach();
   }
 
   onQuery() {
